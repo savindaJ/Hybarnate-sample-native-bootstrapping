1446697652f6804be71a7ef0fc38f243586d579c
SavindaJayasekara <thantrige32@gmail.com>
2023-07-30 13:39:15 +0530

    add style

--- src/main/java/lk/ijse/hybernate/sample/controller/MenuController.java
+++ src/main/java/lk/ijse/hybernate/sample/controller/MenuController.java
@@ -1,10 +1,18 @@
 package lk.ijse.hybernate.sample.controller;
 
+import javafx.animation.ScaleTransition;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 public class MenuController {
     public Label lblTitle;
+    public ImageView imgOrder;
+    public ImageView imgItem;
+    public ImageView imgCustomer;
 
     public void btnCustomerClick(MouseEvent mouseEvent) {
     }
@@ -15,11 +23,50 @@ public class MenuController {
     public void btnOrderClick(MouseEvent mouseEvent) {
     }
 
-    public void mouseenterd(MouseEvent mouseEvent) {
+    public void mouseenterd(MouseEvent event) {
+        if (event.getSource() instanceof ImageView){
+            ImageView view = (ImageView) event.getSource();
 
+            switch (view.getId()){
+                case "imgOrder":
+                    lblTitle.setText("Goto Order Form");
+                    break;
+                case "imgCustomer":
+                    lblTitle.setText("Goto Customer Form");
+                    break;
+                case "imgItem" :
+                    lblTitle.setText("Goto Item Form");
+                    break;
+                default:
+                    break;
+            }
+        }
+        if (event.getSource() instanceof javafx.scene.image.ImageView) {
+            javafx.scene.image.ImageView icon = (ImageView) event.getSource();
+
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1.2);
+            scaleT.setToY(1.2);
+            scaleT.play();
+
+            DropShadow glow = new DropShadow();
+//            glow.setColor(Color.valueOf("#EF233C"));
+            glow.setColor(Color.CORNFLOWERBLUE);
+            glow.setWidth(15);
+            glow.setHeight(15);
+            glow.setRadius(15);
+            icon.setEffect(glow);
+        }
     }
 
-    public void mouseexsits(MouseEvent mouseEvent) {
-
+    public void mouseexsits(MouseEvent event) {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1);
+            scaleT.setToY(1);
+            scaleT.play();
+            icon.setEffect(null);
+        }
     }
 }
