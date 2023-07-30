92c0f3204718a8d57ad73545d51bb45537f5ac67
SavindaJayasekara <thantrige32@gmail.com>
2023-07-30 13:32:03 +0530

    add controller components

--- src/main/java/lk/ijse/hybernate/sample/controller/MenuController.java
+++ src/main/java/lk/ijse/hybernate/sample/controller/MenuController.java
@@ -1,4 +1,25 @@
 package lk.ijse.hybernate.sample.controller;
 
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
 public class MenuController {
+    public Label lblTitle;
+
+    public void btnCustomerClick(MouseEvent mouseEvent) {
+    }
+
+    public void btnItemClick(MouseEvent mouseEvent) {
+    }
+
+    public void btnOrderClick(MouseEvent mouseEvent) {
+    }
+
+    public void mouseenterd(MouseEvent mouseEvent) {
+
+    }
+
+    public void mouseexsits(MouseEvent mouseEvent) {
+
+    }
 }
