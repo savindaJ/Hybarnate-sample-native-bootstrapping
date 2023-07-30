0b6febdc3a1722b9de80cad1c6263d6abf23f81a
SavindaJayasekara <thantrige32@gmail.com>
2023-07-30 13:17:04 +0530

    fix controller

--- src/main/java/lk/ijse/hybernate/sample/controller/HomeController.java
+++ src/main/java/lk/ijse/hybernate/sample/controller/HomeController.java
@@ -1,4 +1,19 @@
 package lk.ijse.hybernate.sample.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class HomeController {
+    @FXML
+    private JFXTextField txtUserName;
+    @FXML
+    private JFXTextField txtPassword;
+    @FXML
+    private JFXButton btnLogin;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+
+    }
 }
