cf8e24ee2516cea33d5c44c376c607e8fbc2f341
SavindaJayasekara <thantrige32@gmail.com>
2023-07-31 11:25:24 +0530

    add custom alert

--- src/main/java/lk/ijse/hybernate/sample/util/CustomAlert.java
+++ src/main/java/lk/ijse/hybernate/sample/util/CustomAlert.java
@@ -1,9 +1,34 @@
 package lk.ijse.hybernate.sample.util;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class CustomAlert extends Alert {
-    public CustomAlert(AlertType alertType) {
-        super(alertType);
+    public CustomAlert(AlertType alertType, String title, String header, String message, ButtonType... buttonTypes) {
+        super(alertType, message, buttonTypes);
+        setTitle(title);
+        setHeaderText(header);
+
+        String image = null;
+        switch (alertType) {
+            case ERROR:
+                image = "/asset/";
+                break;
+            case INFORMATION:
+                image = "/asset/";
+                break;
+            case WARNING:
+                image = "/asset/";
+                break;
+        }
+        if (image !=null){
+            ImageView imgView = new ImageView(new Image(image));
+            imgView.setFitWidth(40);
+            imgView.setFitHeight(40);
+            setGraphic(imgView);
+        }
     }
+
 }
