1737f50db064a520d1d5b2267484ba386ec1e0c3
SavindaJayasekara <thantrige32@gmail.com>
2023-07-31 11:13:43 +0530

    add back navigate

--- src/main/java/lk/ijse/hybernate/sample/controller/ItemController.java
+++ src/main/java/lk/ijse/hybernate/sample/controller/ItemController.java
@@ -2,9 +2,23 @@ package lk.ijse.hybernate.sample.controller;
 
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableView;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class ItemController {
     public JFXTextField txtName;
@@ -13,6 +27,8 @@ public class ItemController {
     public JFXTextField txtCode;
     public JFXComboBox cmbId;
     public TableView tblItem;
+
+    public AnchorPane root;
 
     public void btnSaveOnAction(ActionEvent actionEvent) {
     }
@@ -26,15 +42,50 @@ public class ItemController {
     public void cmbIdOnAction(ActionEvent actionEvent) {
     }
 
-    public void btnBackClicked(MouseEvent event) {
+    public void btnBackClicked(MouseEvent event) throws IOException {
+        Parent root = null;
+        root = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/menu.fxml")));
+        if (root != null) {
+            Scene subScene = new Scene(root);
+            Stage primaryStage = (Stage) this.root.getScene().getWindow();
+            primaryStage.setScene(subScene);
+            primaryStage.centerOnScreen();
 
+            TranslateTransition tt = new TranslateTransition(Duration.millis(350), subScene.getRoot());
+            tt.setFromX(-subScene.getWidth());
+            tt.setToX(0);
+            tt.play();
+
+        }
     }
 
     public void mouseEnterd(MouseEvent event) {
+        if (event.getSource() instanceof javafx.scene.image.ImageView) {
+            javafx.scene.image.ImageView icon = (ImageView) event.getSource();
 
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1.2);
+            scaleT.setToY(1.2);
+            scaleT.play();
+
+            DropShadow glow = new DropShadow();
+//            glow.setColor(Color.valueOf("#EF233C"));
+            glow.setColor(Color.CORNFLOWERBLUE);
+            glow.setWidth(15);
+            glow.setHeight(15);
+            glow.setRadius(15);
+            icon.setEffect(glow);
+        }
     }
 
     public void mouseExsits(MouseEvent event) {
-
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1);
+            scaleT.setToY(1);
+            scaleT.play();
+            icon.setEffect(null);
+        }
     }
 }
