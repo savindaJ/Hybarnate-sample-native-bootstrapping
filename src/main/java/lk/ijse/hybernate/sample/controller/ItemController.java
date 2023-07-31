cfe904f429e9bdeca700d23aa3fc398598f96905
SavindaJayasekara <thantrige32@gmail.com>
2023-07-31 11:09:20 +0530

    fix item controller

--- src/main/java/lk/ijse/hybernate/sample/controller/ItemController.java
+++ src/main/java/lk/ijse/hybernate/sample/controller/ItemController.java
@@ -1,4 +1,36 @@
 package lk.ijse.hybernate.sample.controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
 public class ItemController {
+    public JFXTextField txtName;
+    public JFXTextField txtQty;
+    public JFXTextField txtItemPrice;
+    public JFXTextField txtCode;
+    public JFXComboBox cmbId;
+    public TableView tblItem;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void cmbIdOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBackClicked(MouseEvent event) {
+
+    }
+
+    public void mouseEnterd(MouseEvent event) {
+
+    }
 }
