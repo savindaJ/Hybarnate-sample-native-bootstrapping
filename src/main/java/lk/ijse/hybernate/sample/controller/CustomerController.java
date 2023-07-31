b48d5275d33fbf9baf758197fcbfb1ef95635526
SavindaJayasekara <thantrige32@gmail.com>
2023-07-31 09:57:06 +0530

    add controller in customer form

--- src/main/java/lk/ijse/hybernate/sample/controller/CustomerController.java
+++ src/main/java/lk/ijse/hybernate/sample/controller/CustomerController.java
@@ -1,4 +1,35 @@
 package lk.ijse.hybernate.sample.controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
 public class CustomerController {
+    public JFXTextField txtName;
+    public JFXTextField txtAddress;
+    public JFXTextField txtSalary;
+    public JFXTextField txtId;
+    public JFXComboBox cmbId;
+    public TableView tblCustomer;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void cmbIdOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBackClicked(MouseEvent event) {
+    }
+
+    public void mouseEnterd(MouseEvent event) {
+
+    }
 }
