4c252d125e9c0d959005fb0e0db18f633647a49d
SavindaJayasekara <thantrige32@gmail.com>
2023-08-01 11:23:01 +0530

    add Cell value factory

--- src/main/java/lk/ijse/hybernate/sample/controller/CustomerController.java
+++ src/main/java/lk/ijse/hybernate/sample/controller/CustomerController.java
@@ -14,6 +14,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -52,6 +53,19 @@ public class CustomerController {
     void initialize(){
         initUi();
         setCustomerID();
+        setTbData();
+        setCellValueFactory();
+    }
+
+    private void setCellValueFactory() {
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colNic.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colStatus.setCellValueFactory(new PropertyValueFactory<>("salary"));
+        colGmail.setCellValueFactory(new PropertyValueFactory<>("id"));
+    }
+
+    private void setTbData() {
+
     }
 
     private void setCustomerID() {
