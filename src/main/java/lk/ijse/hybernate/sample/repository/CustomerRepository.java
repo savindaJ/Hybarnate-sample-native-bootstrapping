90503f08cb0d7876460d152b7cbbca66a9783051
SavindaJayasekara <thantrige32@gmail.com>
2023-08-05 14:11:08 +0530

    add save customer in repository !

--- src/main/java/lk/ijse/hybernate/sample/repository/CustomerRepository.java
+++ src/main/java/lk/ijse/hybernate/sample/repository/CustomerRepository.java
@@ -3,6 +3,9 @@ package lk.ijse.hybernate.sample.repository;
 import lk.ijse.hybernate.sample.config.StandardConfig;
 import lk.ijse.hybernate.sample.entity.Customer;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.io.Serializable;
 
 
 public class CustomerRepository {
@@ -12,5 +15,18 @@ public class CustomerRepository {
     Session session;
     public Customer getCustomer(String id){
        return session.get(Customer.class,id);
+    }
+
+    public boolean saveCustomer(Customer customer){
+        Transaction transaction = session.beginTransaction();
+
+        /*returning customer id in serializable type*/
+        Serializable save = session.save(customer);
+
+        transaction.commit();
+
+        session.close();
+
+        return save != null;
     }
 }
